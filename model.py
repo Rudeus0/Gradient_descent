b11c49f4955f59f1c3efe370204090a85e942ef6
Ruthresh <rtrwaran5@gmail.com>
2026-04-29 15:54:01 +0530

    feat: implement linear prediction function (hypothesis)

--- model.py
+++ model.py
@@ -6,3 +6,8 @@ def initialize_params():
     weight = 0.0
     bias = 0.0
     return weight, bias
+
+def predict(X, weight, bias):
+    # Calculates the model's output using the linear formula y = wx + c (bias)
+    # Note: Initial output will be 0.0 until weights are optimized
+    return weight * X + bias
