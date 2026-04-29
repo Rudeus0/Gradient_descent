3c048a90d353ac25b827b4a39ed0ea9a031885dc
Ruthresh <rtrwaran5@gmail.com>
2026-04-29 16:06:25 +0530

    feat: implement compute_loss using numpy mean squared error

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def initialize_params():
     """
     Sets initial values for the model. 
@@ -11,3 +13,9 @@ def predict(X, weight, bias):
     # Calculates the model's output using the linear formula y = wx + c (bias)
     # Note: Initial output will be 0.0 until weights are optimized
     return weight * X + bias
+
+
+def compute_loss(y_pred, y_true):
+    # Calculates the Mean Squared Error (MSE)
+    # Measures the average squared distance between predictions and actual values
+    return np.mean((y_pred - y_true) ** 2)
